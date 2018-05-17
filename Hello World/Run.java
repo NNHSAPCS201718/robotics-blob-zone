2abe2d8a8563efb98689075ad4c0f795bef4d7aa
jperona70 <jacobaperona@gmail.com>
2018-05-17 12:20:24 -0500

    blob car

---
+++ Hello World/Run.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Run here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import lejos.nxt.*;
+
+public class Run
+{
+   public static void main(String[] args)
+   {
+       System.out.println("Program 1");
+       this.waitForPress();
+       
+   }
+}
